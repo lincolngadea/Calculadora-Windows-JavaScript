94d2127082831c2fc7140b1ea76e9964ee136b83
Lincoln Gadéa <lincoln@glinc.com.br>
2020-10-16 20:45:01 -0300

    trabalhando na função clicou

---
+++ scripts/controller/script.js
@@ -0,0 +1,43 @@
+function clicou(n){
+    let numbers =[];
+    return console.log(numbers.push(n));
+     
+    
+}
+function porcento(){
+
+}
+function raizQuadrada(){
+
+}
+function dobro(){
+
+}
+function clearEntry(){
+    
+}
+function clearAll(){
+
+}
+function backspace(){
+
+}
+function divisor(){
+
+}
+function multi(){
+
+}
+function sub(){
+
+}
+function sum(){
+
+}
+function changeSignal(){
+
+}
+function equal(){
+
+}
+
